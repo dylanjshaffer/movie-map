1af8e0ccfdf6178600f45abaec466a0ba6d5c6ce
dylanjshaffer <dylanjshaffer@gmail.com>
2017-05-01 13:23:29 -0500

    add search form and map-genre selector to html

--- js/moviemap.js
+++ js/moviemap.js
@@ -1,5 +1,4 @@
 
-
 var tmdbApi = {
   root: "https://api.themoviedb.org/3",
   token: "24758e2d6d872edf774b8e3777b4d0de",
@@ -12,10 +11,16 @@ var tmdbApi = {
 };
 
 function initMap() {
-  var location = {lat: 28.028290, lng: -82.006587};
+  var location = {lat: 41.850033, lng: -87.6500523};
   var mapProperties = {
-    zoom: 3,
+    zoom: 4,
     center: location
   }
   var map = new google.maps.Map(document.getElementById('map'), mapProperties);
 }
+
+$("#search").submit(function(evt) {
+  evt.preventDefault();
+  var searchTerm = $("#search-term").val();
+  
+})
